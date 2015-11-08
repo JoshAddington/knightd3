deb2b07fc6284dcf8f30e2317907103c0b4a84bd
joshaddington <josh.addington@gmail.com>
2015-11-08 12:23:33 -0500

    add p elements to add or delete from barchart

--- refresher/barchart.js
+++ refresher/barchart.js
@@ -1,18 +1,38 @@
 "use strict";
 
-var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-    11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
+var dataset = [ { key: 0, value: 5 },
+                { key: 1, value: 10 },
+                { key: 2, value: 13 },
+                { key: 3, value: 19 },
+                { key: 4, value: 21 },
+                { key: 5, value: 25 },
+                { key: 6, value: 22 },
+                { key: 7, value: 18 },
+                { key: 8, value: 15 },
+                { key: 9, value: 13 },
+                { key: 10, value: 11 },
+                { key: 11, value: 12 },
+                { key: 12, value: 15 },
+                { key: 13, value: 20 },
+                { key: 14, value: 18 },
+                { key: 15, value: 17 },
+                { key: 16, value: 16 },
+                { key: 17, value: 18 },
+                { key: 18, value: 23 },
+                { key: 19, value: 25 } ];
 var w = 600;
 var h = 250;
 var barPadding = 1;
 var maxValue = 50;
+var value = function(d){ return d.value; }
+var key = function(d){ return d.key; }
 
 var xScale = d3.scale.ordinal()
                     .domain(d3.range(dataset.length))
                     .rangeRoundBands([0, w], 0.05);
 
 var yScale = d3.scale.linear()
-                    .domain([0, d3.max(dataset)])
+                    .domain([0, d3.max(dataset, value)])
                     .range([0, h]);
 
 var svg = d3.select("body")
@@ -21,27 +41,29 @@ var svg = d3.select("body")
             .attr("height", h);
 
 svg.selectAll("rect")
-    .data(dataset)
+    .data(dataset, key)
     .enter()
     .append("rect")
     .attr({
         x: function(d, i){ return xScale(i); },
-        y: function(d){ return h - yScale(d); },
+        y: function(d){ return h - yScale(d.value); },
         width: xScale.rangeBand(),
-        height: function(d){ return yScale(d); },
-        fill: function(d){ return "rgb(0, 0, " + (d * 10) +")"; }
+        height: function(d){ return yScale(d.value); },
+        fill: function(d){ return "rgb(0, 0, " + (d.value * 10) +")"; }
+    })
+    .on("click", function(){
+        sortBars();
     });
 
+
 svg.selectAll("text")
-    .data(dataset)
+    .data(dataset, key)
     .enter()
     .append("text")
-    .text(function(d){
-        return d;
-    })
+    .text(value)
     .attr({
         x: function(d, i){ return xScale(i)  + xScale.rangeBand() / 2; },
-        y: function(d){ return h - yScale(d) + 15; },
+        y: function(d){ return h - yScale(d.value) + 15; },
         "font-family": "sans-serif",
         "font-size": "14px",
         "font-weight": "bold",
@@ -49,29 +71,41 @@ svg.selectAll("text")
         "text-anchor": "middle"
     });
 
-d3.select("p")
+d3.selectAll("p")
     .on("click", function(){
-        // Add new value to dataset
-        var maxValue = 25;
-        var newNumber = Math.floor(Math.random() * maxValue);
-        dataset.push(newNumber);
+        var dataOperation = d3.select(this).attr("id");
 
-        // rescale X axis
+        if (dataOperation === "add") {
+            var maxValue = 25;
+            var newNumber = Math.floor(Math.random()* maxValue);
+            var lastKeyValue = dataset[dataset.length-1].key;
+            console.log(lastKeyValue);
+            dataset.push({
+                key: lastKeyValue + 1,
+                value: newNumber
+            });
+        } else {
+            // Remove an entry from the dataset
+            dataset.shift()
+        }
+
+        // rescale axes
         xScale.domain(d3.range(dataset.length));
+        yScale.domain([0, d3.max(dataset, value)]);
 
         // Select bars of barchart
         var bars = svg.selectAll("rect")
-            .data(dataset);
+            .data(dataset, key);
 
         // Create new bar out of range of svg
         bars.enter()
             .append("rect")
             .attr({
                 x: w,
-                y: function(d){ return h - yScale(d); },
+                y: function(d){ return h - yScale(d.value); },
                 width: xScale.rangeBand(),
-                height: function(d){ return yScale(d); },
-                fill: function(d){ return "rgb(0, 0, " + (d * 10) + ")"; }
+                height: function(d){ return yScale(d.value); },
+                fill: function(d){ return "rgb(0, 0, " + (d.value * 10) + ")"; }
             });
 
         // Transition new bar into view
@@ -79,21 +113,27 @@ d3.select("p")
             .duration(500)
             .attr({
                 x: function(d, i){ return xScale(i); },
-                y: function(d){ return h - yScale(d); },
+                y: function(d){ return h - yScale(d.value); },
                 width: xScale.rangeBand(),
-                height: function(d){ return yScale(d); }
+                height: function(d){ return yScale(d.value); }
             });
+
+        bars.exit()
+            .transition()
+            .duration(500)
+            .attr("x", -xScale.rangeBand())
+            .remove();
 
         // Select all text elements
         var text = svg.selectAll("text")
-            .data(dataset);
+            .data(dataset, key);
 
         // Create new text element out of view
         text.enter()
             .append("text")
             .attr({
                 x: w,
-                y: function(d){ return h - yScale(d) + 15; },
+                y: function(d){ return h - yScale(d.value) + 15; },
                 "font-family": "sans-serif",
                 "font-size": "14px",
                 "font-weight": "bold",
@@ -104,47 +144,29 @@ d3.select("p")
         // Transition text element into view
         text.transition()
             .duration(500)
-            .text(function(d){ return d; })
+            .text(value)
             .attr({
                 x: function(d, i) {	return xScale(i) + xScale.rangeBand() / 2; },
-                y: function(d){ return h - yScale(d) + 14; }
+                y: function(d){ return h - yScale(d.value) + 14; }
             });
-        // // create new values for the dataset
-        // var numValues = dataset.length;
-        // dataset = [];
-        // for (let i = 0; i < numValues; i++){
-        //     dataset.push(Math.floor(Math.random() * maxValue) + 1);
-        // }
-        //
-        // // update yScale based on highest number in new dataset
-        // yScale.domain([0, d3.max(dataset)]);
-        //
-        // // update svg rect with new data values
-        // svg.selectAll("rect")
-        //     .data(dataset)
-        //     .transition()
-        //     .delay(function(d, i){
-        //         return i/dataset.length * 1000;
-        //     })
-        //     .duration(500)
-        //     .attr({
-        //         y: function(d){
-        //             console.log(d);
-        //             return h - yScale(d); },
-        //         height: function(d){ return yScale(d); },
-        //         fill: function(d){ return "rgb(0, 0, " + d * 10 + ")"}
-        //     });
-        //
-        // // update labels with new values
-        // svg.selectAll("text")
-        //     .data(dataset)
-        //     .transition()
-        //     .delay(function(d, i){
-        //         return i / dataset.length * 1000;
-        //     })
-        //     .duration(500)
-        //     .text(function(d){ return d; })
-        //     .attr({
-        //         y: function(d){ return h - yScale(d) + 14; }
-        //     })
+
+        text.exit()
+            .transition()
+            .duration(500)
+            .attr("x", -xScale.rangeBand())
+            .remove()
     });
+
+    var sortBars = function() {
+
+            svg.selectAll("rect")
+               .sort(function(a, b) {
+                     return d3.ascending(a.value, b.value);
+               })
+               .transition()
+               .duration(1000)
+               .attr("x", function(d, i) {
+                     return xScale(i);
+               });
+
+    };
